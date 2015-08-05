89e9d22ca5358fad7b732551c86746e4ae757e6f
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-08-05 11:43:07 +0100

    Added REST

---
+++ REST.rb
@@ -0,0 +1,20 @@
+#REST stands for Representational State Tranfer
+
+#It is an software architectural style that is often
+#used in the development of Web Services. It uses simple HTTP
+#to make calls between machines rather than complex mechanisms
+
+#REST is often preferred to approaches such as COBRA or SOAP
+#for the following reasons:
+
+#Consumes less bandwidth
+#Stateless
+#Easier to handle by the receiver
+
+#Sinatra works well with REST because it supports:
+
+#All HTTP verbs(e.g. GET, POST, PUT, DELETE) - these are methods
+#Caching
+#Content types
+#Routes (the verb method takes the route as a parameter)
+
